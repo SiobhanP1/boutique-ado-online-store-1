e024c333f8af20487e9740040ff380d45a6293dd
SiobhanP1 <siobhanphillips11@gmail.com>
2023-11-18 15:21:49 +0000

    Add links and urls for bag view template

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_bag(request):
+    """A view to show contents of shopping cart"""
+    return render(request, 'bag/bag.html')
