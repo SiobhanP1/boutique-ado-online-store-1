bf9011818c79278a63cc64b46cf6709cd4cf7495
SiobhanP1 <siobhanphillips11@gmail.com>
2023-11-22 22:00:09 +0000

    Create remove from bag and adjust bag views and urls

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,61 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
 def view_bag(request):
     """A view to show contents of shopping cart"""
     return render(request, 'bag/bag.html')
+
+
+def adjust_bag(request, item_id):
+    """Adjust quantity of specified product to specified amount"""
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else: 
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the bag"""
+    
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else: 
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        print(request.session['bag'])
+        #As posting from javascript function, use HttpResponse to return 200 (success)
+        return HttpResponse(status=200)
+    except:
+        return HttpResponse(status=500)
+
 
 def add_to_bag(request, item_id):
     """Add a quantity of specified item to shopping bag"""
