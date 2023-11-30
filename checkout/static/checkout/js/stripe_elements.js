d95c01d2e3e62b544a8c556b7455ebbe1e60e6f8
SiobhanP1 <siobhanphillips11@gmail.com>
2023-11-30 20:23:12 +0000

    Core checkout functionality added. can submit payments via stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,9 +6,9 @@
 * https://stripe.com/docs/js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
 /* Must define style before creating card if doing this way*/
@@ -16,28 +16,70 @@ var elements = stripe.elements();
 /* var card = elements.create('card', {style: style});*/
 
 /* CSS below directly from stripe.com */
-var element = elements.create('card', {
+var card = elements.create('card', {
     style: {
       base: {
-        iconColor: '#c4f0ff',
-        color: '#fff',
+        iconColor: '#000',
+        color: '#000',
         fontWeight: '500',
         fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
         fontSize: '16px',
         fontSmoothing: 'antialiased',
         ':-webkit-autofill': {
-          color: '#fce883',
+          color: '#000',
         },
         '::placeholder': {
-          color: '#87BBFD',
+          color: '#000',
         },
       },
       invalid: {
-        iconColor: '#FFC7EE',
-        color: '#FFC7EE',
+        iconColor: '#FF0000',
+        color: '#FF0000',
       },
     },
   });
 
-card.mount('#card-element');
 
+// Handle realtime validation errors on card element
+
+card.addEventListener('change', function(event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = 
+            `<span>${event.error.message}</span>`
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+    }
+)
+
+// Handle form submit event
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  card.update({'disabled': true});
+  $('#submit-button').attr('disabled', true);
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+    }
+  }).then(function(result) {
+    if (result.error) {
+      var errorDiv = document.getElementById('card-errors');
+      var html = 
+        `<span>${result.error.message}</span>`
+      $(errorDiv).html(html);
+      card.update({'disabled': false});
+      $('#submit-button').attr('disabled', false);
+    } else {
+      if (result.paymentIntent.status === 'succeeded') {
+        form.submit();
+      }
+    }
+  })
+})
+
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'Nothing in bag')
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount = stripe_total,
+        currency = settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning('No stripe public key.')
+
     template = 'checkout/check_out.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OHuq7B93WdcdSYuxhoUQzkacDz38iQQwFUwS3Ml9kP1nHSbNwg77meRFHs6w7gE2nYgP1wvCY2M5bXpuPxBeEqa00YckBBfTt',
-        'client_secret': 'test secret key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
