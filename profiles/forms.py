a4f422e643e876e18d8171334880ac1effe1bab9
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-03 23:08:01 +0000

    Add profile view

---
+++ profiles/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import UserProfile
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = (user,)
+
+    def __init__(*args, **kwargs):
+        """Add placeholders and classes. Autofocus on first field.""" 
+        super().__init__(self, *args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postcode',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus']=True
+        for field in self.fields:
+            if field != 'default_country':
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+        self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+        self.fields[field].label = False
