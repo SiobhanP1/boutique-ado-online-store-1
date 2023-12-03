c4818973029602a93dd6d9fca9598a86cdbf643a
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-03 16:40:44 +0000

    Add country drop down to checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -3,19 +3,20 @@ import uuid
 from django.db import models
 from django.conf import settings
 from django.db.models import Sum
+from django_countries.fields import CountryField
 
 from products.models import Product
 
 
 # Create your models here.
 
-class Order(models.Model):
+class Order(models.Model, CountryField):
 
     order_number = models.CharField(max_length=2, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=284, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=False, blank=False)
