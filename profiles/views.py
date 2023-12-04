51b1dfa019bba63fb75000368093488f4eaae5e2
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-04 21:46:56 +0000

    Add add_product links to base and mobile templates

--- profiles/views.py
+++ profiles/views.py
@@ -13,8 +13,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated.')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Could not update profile.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
