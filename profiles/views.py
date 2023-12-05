985d6510f5ab23b6c07e827b53af57c727fe55cc
SiobhanP1 <siobhanphillips11@gmail.com>
2023-12-05 13:17:04 +0000

    Create custom file input widget file and class

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
-from django.contrib import messages
 
+
+@login_required
 def profile(request):
     """Diplays user profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
